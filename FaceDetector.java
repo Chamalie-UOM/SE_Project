d5b26bb743938dcc8d8b3a77e5b8e4adf8e49532
Chamalie-UOM <chamalie.15@cse.mrt.ac.lk>
2018-02-27 16:44:07 +0530

    Add files via upload

--- FaceDetector.java
+++ FaceDetector.java
@@ -1,4 +1,4 @@
-package authentication;
+package GUAclasses;
 
 import java.awt.BorderLayout;
 
@@ -13,11 +13,13 @@ import java.awt.event.ActionEvent;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.beans.PropertyVetoException;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.videoio.VideoCapture;
 import static org.opencv.imgcodecs.Imgcodecs.imencode; 
 import org.opencv.imgproc.Imgproc;
@@ -26,23 +28,23 @@ import java.sql.*;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import java.awt.Font;
+import javax.swing.JInternalFrame;
+import java.awt.Color;
 public class FaceDetector extends JFrame {
 	
 	//definitions
 	private DaemonThread thread1 = null;
-	int count =0;
-	Mat frame;
-	MatOfByte byteFrame;
-	CascadeClassifier faceTracker;
-	MatOfRect trackedFaces;
-	VideoCapture webSource;
+	private Mat frame;
+	private MatOfByte byteFrame;
+	private CascadeClassifier faceTracker;
+	private MatOfRect trackedFaces;
+	private VideoCapture webSource;
 	MysqlConnection db = new MysqlConnection();
 	User newuser;
-	//Connection con = db.dbConnect();
 	
 	class DaemonThread implements Runnable{
 		protected volatile boolean Runnable= false;
-		
+		boolean dbflag = false;
 		public void run() {
 			synchronized(this) {
 				while(Runnable) {
@@ -51,27 +53,27 @@ public class FaceDetector extends JFrame {
 							this.detectFace();
 							Graphics g = contentPane.getGraphics();
 							this.drawRectangle(trackedFaces);
-							this.displayFrame(frame, g);
-							if (db.conn!= null) {
-								//Statement st =con.createStatement();
-								//st.executeUpdate("insert into user(name, email) values('what','the@gmail.com');");
+							this.displayFrame(frame, g);	
+							
+							// adding new user data to the database.
+							if (db.conn!= null && !dbflag) {
 								System.out.println("reached here.");
+								dbflag =true;
 								newuser = new User();
 								newuser.setName(textFieldname.getText());
 								newuser.setMail(textFieldmail.getText());
 								newuser.setTpnum(textFieldtpnum.getText());
 								newuser.setRecovPass(textFieldPass.getText());
-								db.saveData(newuser);	
-							}/*else {
-								System.out.println("connection is null.");
+								newuser.setFace(extractFace(trackedFaces,newuser));
+								db.saveData(newuser);
 							}
-							*/
+							
 						}catch (Exception ex) {
-							System.out.println(ex);
+							System.out.println(ex +"1");
 						}
-					
 					}
 				}
+				
 			}
 		}
 		
@@ -80,6 +82,7 @@ public class FaceDetector extends JFrame {
 		public void drawRectangle(MatOfRect trackedFaces) {
 			for(Rect rect : trackedFaces.toArray()) {
 				Imgproc.rectangle(frame, new Point(rect.x,rect.y), new Point(rect.x + rect.width,rect.y+rect.height), new Scalar(255, 255, 0), 2);
+				
 			}
 		}
 		
@@ -104,10 +107,27 @@ public class FaceDetector extends JFrame {
 				BufferedImage buff =(BufferedImage) im;
 				//display the frames on the panel
 				g.drawImage(buff, 10, 10, getWidth()-30, getHeight() - 185, 0, 0, buff.getWidth(),buff.getHeight(), null);
+				
 			} catch (IOException e) {
 				e.printStackTrace();
 			}		
 		}
+		
+		//extract the face part from the frame and resize
+		public Mat extractFace(MatOfRect faces, User user) throws IOException {
+			Mat face=null;
+			for(Rect rect : faces.toArray()) {
+				face = frame.submat(rect);
+				Imgproc.cvtColor(face, face, Imgproc.COLOR_BGR2GRAY);
+				Size sz = new Size(240,240);
+				Imgproc.resize(face, face, sz);
+				String filename= user.getName()+".jpg" ;
+				Imgcodecs.imwrite(filename,face);	
+			}
+			System.out.print(face);
+			return face;
+		}
+		
 		
 	}
 	
@@ -138,17 +158,52 @@ public class FaceDetector extends JFrame {
 	 * Create the frame.
 	 */
 	public FaceDetector() {
+		setTitle("Registration");
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 629, 503);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(240, 230, 140));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JPanel panel = new JPanel();
-		panel.setBounds(78, 11, 411, 36);
-		contentPane.add(panel);
+		JInternalFrame secPage = new JInternalFrame("Create Graphical Password");
+		secPage.setClosable(true);
+	
+		secPage.setBounds(0, 1, 534, 438);
+		contentPane.add(secPage);
+		secPage.getContentPane().setLayout(null);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(90, 24, 330, 317);
+		secPage.getContentPane().add(panel_1);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(208, 341, 89, 23);
+		secPage.getContentPane().add(btnNewButton);
+		secPage.setVisible(false);
+		
+		JButton setPass = new JButton("Set Password");
+		setPass.setVisible(false);
+		setPass.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				thread1.Runnable=false;
+				//secPage.setVisible(true);			
+				//secPage.toFront();
+				FaceDetector.this.dispose();
+				webSource.release();
+				CreatePass frame1 = new CreatePass(newuser);
+				frame1.setVisible(true);
+				
+				
+				
+				
+			}
+		});
+		setPass.setFont(new Font("Georgia", Font.PLAIN, 15));
+		setPass.setBounds(260, 416, 147, 23);
+		contentPane.add(setPass);
 		
 		JButton Nextbtn = new JButton("Next");
 		Nextbtn.setFont(new Font("Georgia", Font.PLAIN, 15));
@@ -170,9 +225,11 @@ public class FaceDetector extends JFrame {
 				textFieldmail.setVisible(false);
 				textFieldtpnum.setVisible(false);
 				textFieldPass.setVisible(false);
+				Nextbtn.setVisible(false);
+				setPass.setVisible(true);
 			}
 		});
-		Nextbtn.setBounds(249, 430, 147, 23);
+		Nextbtn.setBounds(260, 382, 147, 23);
 		contentPane.add(Nextbtn);
 		
 		textFieldname = new JTextField();
@@ -214,5 +271,7 @@ public class FaceDetector extends JFrame {
 		lblRecoveryPassword.setFont(new Font("Georgia", Font.PLAIN, 15));
 		lblRecoveryPassword.setBounds(78, 229, 133, 36);
 		contentPane.add(lblRecoveryPassword);
+	
+		
 	}
 }
